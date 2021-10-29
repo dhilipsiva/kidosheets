51d79b62effa4cb2872a3e543d189fdb69ab1737
dhilipsiva <dhilipsiva@pm.me>
2021-10-30 01:05:01 +0530

    minor UX improvements

--- kidosheets/core.py
+++ kidosheets/core.py
@@ -10,7 +10,7 @@ def cli():
 
 
 @cli.command()
-@click.option("--root_val", default="")
+@click.option("--sign", default="?")
 @click.option("--min_rows", default=0, type=int)
 @click.option("--max_rows", default=10, type=int)
 @click.option("--step_rows", default=1, type=int)
@@ -19,7 +19,7 @@ def cli():
 @click.option("--step_cols", default=1, type=int)
 @click.option("--filename", default="kidosheets.xlsx")
 def math_square_ruled(
-    root_val, min_rows, max_rows, step_rows, min_cols, max_cols, step_cols, filename
+    sign, min_rows, max_rows, step_rows, min_cols, max_cols, step_cols, filename
 ):
     workbook = xlsxwriter.Workbook(filename)
 
@@ -34,14 +34,21 @@ def math_square_ruled(
     cell_format.set_align("center")
     cell_format.set_align("vcenter")
 
-    worksheet.write(0, 0, root_val, cell_format)
+    worksheet.write(0, 0, sign, cell_format)
     for row in range(min_rows, max_rows, step_rows):
         worksheet.write(row + 1, 0, row, cell_format)
     for col in range(min_cols, max_cols, step_cols):
         worksheet.write(0, col + 1, col, cell_format)
-    for row in range(min_rows + 1, max_rows + 1, step_rows):
-        for col in range(min_cols + 1, max_cols + 1, step_cols):
-            worksheet.write(row, col, "", cell_format)
+
+    cell_format = workbook.add_format()
+    cell_format.set_border(7)
+    cell_format.set_font_size(7)
+    cell_format.set_align("center")
+    cell_format.set_align("top")
+
+    for row in range(min_rows, max_rows, step_rows):
+        for col in range(min_cols, max_cols, step_cols):
+            worksheet.write(row + 1, col + 1, f"{row} {sign} {col}", cell_format)
 
     workbook.close()
 
